920b5c8b7b0a0f2b2771d5270ef55c9c25c66605
Jennifer Liu <jnnfrliu33@gmail.com>
2017-10-17 22:34:38 +0800

    Handle case for no name entered

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -31,7 +31,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func displayLyrics(_ sender: Any) {
-        lyricsView.text! = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+        if nameField.text != "" {
+            lyricsView.text! = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+        } else {
+            print ("No name was entered.")
+        }
     }
 }
 
